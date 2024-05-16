2bb9673e47c61bb8c2f8c87243fdc55a34d2bf1b
paper23 <carta.1996206@studenti.uniroma1.it>
2024-05-16 23:11:35 +0200

    Axios e CORS funzionanti

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -7,13 +7,24 @@ export default {}
 
 <template>
 
-	<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-		<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#/">Example App</a>
-		<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-	</header>
+	<div>
+		<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+			<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#/">WASAPhoto</a>
+			<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+		</header>
 
+		<div class="container-fluid">
+			<div class="row">
+				<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+					<RouterView />
+				</main>
+			</div>
+		</div>
+
+	</div>
+<!--
 	<div class="container-fluid">
 		<div class="row">
 			<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
@@ -28,7 +39,7 @@ export default {}
 								Home
 							</RouterLink>
 						</li>
-						<li class="nav-item">
+						<li class="nav-item">''
 							<RouterLink to="/link1" class="nav-link">
 								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#layout"/></svg>
 								Menu item 1
@@ -61,6 +72,7 @@ export default {}
 			</main>
 		</div>
 	</div>
+-->
 </template>
 
 <style>
